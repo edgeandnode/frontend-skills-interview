e61526a3a3551178a9a90168e6ab5c289ee77dc1
Chris Whited <cmwhited1990@gmail.com>
2025-02-04 10:32:14 -1000

    fix(build): fix pino-pretty build issues with wagmi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  webpack(config) {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.externals.push('pino-pretty', 'encoding')
 
-export default nextConfig;
+    return config
+  },
+}
+
+export default nextConfig
