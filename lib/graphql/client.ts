9d620ed1baa039055b050d97e8e03cba7fec23bc
Andras Szucs <andras@andras.codes>
2025-02-04 21:56:25 +0100

    fix(env): fix passing the ROOT_URI

--- lib/graphql/client.ts
+++ lib/graphql/client.ts
@@ -2,4 +2,4 @@ import { env } from '@/lib/env/server'
 import { GraphQLClient } from 'graphql-request'
 
 // on the client use the /graphql endpoint
-export const client = new GraphQLClient(`${env.ROOT_URI}/graphql`)
+export const client = new GraphQLClient(`${env.NEXT_PUBLIC_ROOT_URI}/graphql`)
